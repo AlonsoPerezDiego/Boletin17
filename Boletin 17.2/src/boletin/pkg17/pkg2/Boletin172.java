d792dc71bcb17dbb32be9408d2123765fafc38a2
AlonsoPerezDiego <Dialpe22@gmail.com>
2017-02-01 08:48:17 +0100

    Boletín 17.2

---
+++ Boletin 17.2/src/boletin/pkg17/pkg2/Boletin172.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg17.pkg2;
+
+/**
+ *
+ * @author Diego
+ */
+
+public class Boletin172 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Notas listado = new Notas();
+        
+        listado.pedirNotas();
+        listado.mostrar();
+    }
+    
+}
---
+++ Boletin 17.2/src/boletin/pkg17/pkg2/Notas.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin.pkg17.pkg2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Diego
+ */
+public class Notas {
+    int [] notas = new int[30];
+    
+    public void pedirNotas(){
+        for(int i = 0; i<notas.length; i++){
+            try{
+                notas[i] = Integer.parseInt(JOptionPane.showInputDialog("Introduce nota del alumno nº " + (i+1)));
+            }catch(Exception ex){
+                System.out.println("Valor invalido.");
+            }
+        }
+    }
+    
+    public void mostrar(){
+        for(int i =0; i<notas.length; i++){
+            System.out.println(notas[i]);
+        }
+    }
+}
